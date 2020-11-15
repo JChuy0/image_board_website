86a2f7a2946f8b42fce3ef2c27b2cadc8f48003b
J <skorchkirby0@gmail.com>
2020-11-14 23:02:11 -0600

    Working on adding user comments
    I am working on adding comment functionality, so that users can comment on posts made by other users.
    
    I've added a few test comments to the database. But if a post has multiple comments, only the first comment is displayed.

--- show.php
+++ show.php
@@ -10,11 +10,20 @@ This script lets you see the entire message of a post.
 
     if((filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT)) ) {
         $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
-    
-        $query = "SELECT * FROM Dioramas WHERE Diorama_ID = :id";
+
+        $query = "SELECT dioramas.*, comments.User_Comment, users.Username
+                    FROM dioramas 
+                        LEFT JOIN comments ON dioramas.Diorama_ID = comments.Diorama_ID  
+                        LEFT JOIN users ON users.User_ID = comments.User_ID
+                        WHERE Dioramas.Diorama_ID = :id";
         $statement = $db->prepare($query);
         $statement->bindvalue(':id', $id, PDO::PARAM_INT);
         $statement->execute();
+
+        $row = $statement->fetch();
+
+        print_r($row);
+        
 
         if($statement->rowcount() === 0) {
             header("Location: index.php");
@@ -51,15 +60,22 @@ This script lets you see the entire message of a post.
 
 <div id="all_blogs">
 
-    <?php while($row = $statement->fetch() ) : ?>
+<fieldset>
+    <?php ($row = $statement->fetch() ) ?>
 
-        <div class="blog_post">
+    <div class="blog_post">
 
         <h2><a href="show.php?id=<?= $row['Diorama_ID'] ?>"><?= $row['Title'] ?></a></h2>
         <p>
             <small>
+            Posted by - <?= $row['Username'] ?> <br>
             <?= date('F d, Y, h:i a', strtotime($row['Date_Posted'])) ?> -
             <a href="edit.php?id=<?= $row['Diorama_ID'] ?>">edit</a>
+
+            <?php if($row['Date_Edited'] !== NULL) : ?>
+                <br>Last Edited - <?= date('F d, Y, h:i a', strtotime($row['Date_Edited'])) ?>
+            <?php endif ?>
+
             </small>
         </p>
             <div class='blog_content'>
@@ -69,9 +85,25 @@ This script lets you see the entire message of a post.
         <?php if (strlen($row['Image_Name']) > 1) : ?>
             <img src="Uploads/<?= $row['Image_Name']?>" alt="<?= $row['Image_Name'] ?>" />
         <?php endif ?>
-        </div> <!-- END div class="blog_post" -->
-    <?php endwhile ?>
 
+    </div> <!-- END div class="blog_post" -->
+</fieldset>
+
+<fieldset>
+    <form action="process.php" method="post">
+        <label for="comment">Add a Comment</label>
+        <textarea name="comment" id="comment"></textarea>
+
+        <input type="hidden" name="diorama_id" value="<?= $id?>" />
+        <input type="hidden" name="user_id" value="<?= $id?>" />
+        <input type="submit" name="command" value="Comment" />
+    </form>
+
+    <fieldset>
+        <?= $row['User_Comment'] ?>
+    </fieldset>
+
+</fieldset>
 </div> <!-- END div id="all_blogs" -->
 
 
